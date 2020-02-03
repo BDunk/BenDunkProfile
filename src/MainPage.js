0700cf4578df2a58f599179367f8e3175736268c
benjamin dunk <benkdunk@gmail.com>
2020-02-03 16:05:22 -0500

    rejigged the appbar

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,25 +1,48 @@
 import React from 'react';
-
+import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import {Toolbar, Button} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
 import {
     Link
 } from 'react-router-dom'
 
-import {Button} from "@material-ui/core";
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        alignItems: 'center',
+    },
+    menu_bar_item: {
+        flexGrow: 1,
+    },
+}));
 
 function MainPage(props) {
     console.log(`Props are ${JSON.stringify(props)}`);
 
+    const classes = useStyles();
     return (
-        <div>
-            Main Page
-
-            {/*todo: can update history directly */}
-            <Button
-                onClick = {() => props.history.push('/projects')}
-            >
-                Go to Projects
-            </Button>
-        </div>
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <div className={classes.menu_bar_item}>
+                            Ben Dunk
+                        </div>
+                        <div className={classes.menu_bar_item}>
+                            About
+                        </div>
+                        <div className={classes.menu_bar_item}>
+                            Projects
+                        </div>
+                        <div className={classes.menu_bar_item}>
+                            Ramblings
+                        </div>
+                        <div className={classes.menu_bar_item}>
+                            Contact
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
     );
 }
 
