9708ee921db2d94e4720d45aaff22473e5656f1a
benjamin dunk <benkdunk@gmail.com>
2020-03-31 19:45:45 -0400

    added new photos

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import '../App.css';
 import TopBar from '../Components/TopBar.js';
-import head_shot from '../Images/0.jpeg'
+import head_shot from '../Images/1.jpg'
+import {makeStyles} from "@material-ui/core";
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+    },
+    photo: {
+        marginLeft: "15px",
+        marginRight: "25px",
+    },
+
+}));
 
 
 function MainPage(props) {
+    const classes = useStyles();
     return (
             <div>
                 <header>
                 <TopBar />
                 </header>
-                <div>
-                    <img src={head_shot} alt="Logo" />
+                <div className={classes.photo}>
+                    <img
+                        
+                        src={head_shot}
+                        alt="Logo"
+                    />
                         My name is Ben Dunk
                 </div>
 
